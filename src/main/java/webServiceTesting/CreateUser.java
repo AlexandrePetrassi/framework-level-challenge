5effd8064e34fc98ad3547f1dd3466a8079774e4
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2021-04-02 22:35:46 -0300

    Add: CreateUser missing implementation
    * Fix: CreateUser#buildBody
    
    * Add: CreateUser#setSurname
    
    * Add: CreateUser#getFullName
    
    * Fix: CreateUser#buildBodyWithSurname
    
    * Test: CreateUser#setSurname
    
    Added a unit test to cover setSurname through buildBodyWithSurname by
    setting a custom surname instead of using the default one.

--- src/main/java/webServiceTesting/CreateUser.java
+++ src/main/java/webServiceTesting/CreateUser.java
@@ -5,8 +5,14 @@ import io.restassured.specification.RequestSpecification;
 
 public class CreateUser {
 
+  private static final String USER_AND_JOB_JSON_TEMPLATE = "{\n" +
+          "    \"name\": \"%s\",\n" +
+          "    \"job\": \"%s\"\n" +
+          "}";
+
   private String name;
   private String job;
+  private String surname = "autoSurname";
   private final RequestSpecification requestSpecification;
 
   public CreateUser() {
@@ -27,11 +33,19 @@ public class CreateUser {
     this.job = job;
   }
 
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public String getFullName() {
+    return name + " " + surname;
+  }
+
   public String buildBody() {
-    return null;
+    return String.format(USER_AND_JOB_JSON_TEMPLATE, name, job);
   }
 
   public String buildBodyWithSurname() {
-    return null;
+    return String.format(USER_AND_JOB_JSON_TEMPLATE, getFullName(), job);
   }
 }

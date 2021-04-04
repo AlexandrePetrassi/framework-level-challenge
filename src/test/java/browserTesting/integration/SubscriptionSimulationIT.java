ae7b607a32d732826d3ea11c493051b9f0de3d48
alexandre.cardoso <alexandrepetrassi@gmail.com>
2021-04-04 15:58:27 -0300

    Add: SubscriptionSimulationIT runner

---
+++ src/test/java/browserTesting/integration/SubscriptionSimulationIT.java
@@ -0,0 +1,15 @@
+package browserTesting.integration;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"classpath:features"},
+        glue = {"browserTesting"},
+        strict = true,
+        tags = "@SubscriptionSimulation"
+)
+public class SubscriptionSimulationIT {
+}
